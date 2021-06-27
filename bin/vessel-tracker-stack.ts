6cbd67c9ca61322f9127263d2501e3e3a33b0192
hbrooks <seanhunterbrooks@gmail.com>
2021-06-27 09:51:50 -0400

    allow deploys to happen on commit to "main"; fix bug to load env var into stack name;

--- bin/vessel-tracker-stack.ts
+++ bin/vessel-tracker-stack.ts
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
-import { triggerAsyncId } from 'async_hooks';
 import { VesselTrackerStack } from '../lib/vessel-tracker-stack';
 
-declare var process : {
-    env: {
-        TPI_PUBLIC_INTERVIEW_ID: string
-    }
+
+let tpiPublicInterviewId: string
+if (process.env.TPI_PUBLIC_INTERVIEW_ID) {
+    tpiPublicInterviewId = process.env.TPI_PUBLIC_INTERVIEW_ID
+} else {
+  throw new Error("TPI_PUBLIC_INTERVIEW_ID environment variable is not set.")
 }
 
 const app = new cdk.App();
-new VesselTrackerStack(app, 'VesselTracker', process.env["TPI_PUBLIC_INTERVIEW_ID"], {
+new VesselTrackerStack(app, 'VesselTracker', tpiPublicInterviewId, {
     description: "Do not modify manually!  Stack contains a Vessle Tracker API service accessable by an AWS Application Load Balancer.",
     tags: {
         'is_candidate_owned': 'true',
         'tpi_app_name': 'VesselTracker',
+        'tpi_public_interview_id': tpiPublicInterviewId,
     },
     terminationProtection: false,
 });
