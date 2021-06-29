fee4c384c8fefd8f8e8b4fbc750f4fd9c397d61d
hbrooks <seanhunterbrooks@gmail.com>
2021-06-29 18:50:43 -0400

    health check passes;

--- lib/vessel-tracker-stack.ts
+++ lib/vessel-tracker-stack.ts
@@ -3,7 +3,9 @@ import * as path from 'path';
 
 import * as cdk from "@aws-cdk/core";
 import * as ec2 from "@aws-cdk/aws-ec2";
-import * as ecs from "@aws-cdk/aws-ecs";
+import * as lambda from '@aws-cdk/aws-lambda';
+import { EcrImageCode, Handler, Runtime } from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 import { DockerImageAsset } from '@aws-cdk/aws-ecr-assets';
 
@@ -15,37 +17,31 @@ export class VesselTrackerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, disambiguator: string, props?: cdk.StackProps) {
     super(scope, id+'-'+disambiguator, props);
 
-    const vpc = new ec2.Vpc(this, "Vessel Service VPC", {
-      maxAzs: 2 // Default is all AZs in region
+    const lambdaImage = EcrImageCode.fromAssetImage(path.join(__dirname, '..', 'vessel_tracker_service'));
+
+    const handler = new lambda.Function(this, "Lambda", {
+      code: lambdaImage,
+      handler: Handler.FROM_IMAGE,
+      runtime: Runtime.FROM_IMAGE,
     });
 
-    const cluster = new ecs.Cluster(this, "Vessel Service ECS", {
-      vpc: vpc,
+    const api = new apigateway.RestApi(this, "Rest API GW", {
+      restApiName: 'VesselService-' + disambiguator,
+      description: "Don't modify manually!  Automatically created by CDK via a push to GitHub."
     });
 
-    const image = new DockerImageAsset(this, 'Vessel Service Docker Image', {
-      directory: path.join(__dirname, '..', 'vessel_tracker_service')
+    const lambdaIntegration = new apigateway.LambdaIntegration(handler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
     });
 
-    // Create a load-balanced Fargate service and make it public
-    const vesselService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "Vessel Service", {
-      cluster: cluster, // Required
-      cpu: 256, // Default is 256
-      desiredCount: 1, // Default is 1
-      taskImageOptions: { 
-        image: ecs.ContainerImage.fromDockerImageAsset(image),
-        containerPort: 5000,
-      },
-      minHealthyPercent: 50, 
-      maxHealthyPercent: 200,
-      memoryLimitMiB: 512, // Default is 512
-      publicLoadBalancer: true, // Default is false
-      serviceName: 'VesselService-' + disambiguator,
-    });
+    const apiGwProxyResource = new apigateway.Resource(this, 'ApiGatewayResource', {
+      parent: api.root,
+      pathPart: '{proxy+}'
+    })
+    apiGwProxyResource.addMethod('ANY', lambdaIntegration)
 
     new cdk.CfnOutput(this, "ApiUrl", {
-      // TODO: Fix this hacky addition of "http://"
-      value: 'http://' + vesselService.loadBalancer.loadBalancerDnsName
+      value: api.url
     });
 
   }
--- vessel_tracker_service/api/__init__.py
+++ vessel_tracker_service/api/__init__.py
@@ -5,8 +5,8 @@ from flask import Flask
 from flask import request
 from werkzeug.exceptions import UnprocessableEntity
 
-from .src.request_parser import RequestParser
-from .src.trip_update import TripUpdate
+from .request_parser import RequestParser
+from .trip_update import TripUpdate
 
 
 LOG = logging.getLogger()
@@ -14,20 +14,20 @@ LOG.setLevel(logging.INFO)
 
 
 request_parser = RequestParser()
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@flask_app.route('/health', methods=['GET'])
+@app.route('/health', methods=['GET'])
 def handle_health_check():
     return {"isHealthy": True,}, 200
 
 
-@flask_app.route('/trip', methods=['POST'])
+@app.route('/trip', methods=['POST'])
 def handle_trip_update():
     update: TripUpdate = request_parser.parse_trip_update_event(request.body)
     return update.to_dict(), 200
 
 
-@flask_app.errorhandler(UnprocessableEntity)
+@app.errorhandler(UnprocessableEntity)
 def handle_unprocessable_entity(e):
     return {'message': 'UnprocessableEntity'}, 422
